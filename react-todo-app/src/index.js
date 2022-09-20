7982fa1668bafcfb9a833bff005985f8d27217da
Simon Garcia <sgarcia@neural.com.do>
2022-09-20 14:26:42 -0400

    Put TodoContainer on Strict Mode

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -4,4 +4,9 @@ import ReactDom from "react-dom"
 //component file
 import TodoContainer from "./components/TodoContainer"
 
-ReactDom.render(<TodoContainer />, document.getElementById("root"))
+ReactDom.render(
+<React.StrictMode>
+    <TodoContainer />
+</React.StrictMode>, 
+document.getElementById("root")
+)
