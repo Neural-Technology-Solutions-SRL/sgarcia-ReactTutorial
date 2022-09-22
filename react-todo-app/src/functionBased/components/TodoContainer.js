83d3283c0a4da7039e283bc13a804ebd736543cb
Simon Garcia <sgarcia@neural.com.do>
2022-09-22 16:04:56 -0400

    Converted TodoContainer from class to functional component

--- react-todo-app/src/functionBased/components/TodoContainer.js
+++ react-todo-app/src/functionBased/components/TodoContainer.js
@@ -1,35 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import TodosList from "./TodosList";
 import InputTodo from "./InputTodo";
 import Header from "./Header";
 import { v4 as uuidv4 } from "uuid";
 
-class TodoContainer extends React.Component {
-  state = {
-    todos: [],
-  };
+const TodoContainer = () => {
+  const [todos, setTodos] = useState([]);
 
-  componentDidMount() {
-    const temp = localStorage.getItem("todos");
-    const loadedTodos = JSON.parse(temp);
-    if (loadedTodos) {
-      this.setState({
-        todos: loadedTodos,
-      });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.todos !== this.state.todos) {
-      const temp = JSON.stringify(this.state.todos);
-      localStorage.setItem("todos", temp);
-    }
-  }
-
-  handleChange = (id) => {
-    this.setState((prevState) => ({
+  const handleChange = (id) => {
+    setTodos((prevState) =>
       //Updater function
-      todos: prevState.todos.map((todo) => {
+      prevState.map((todo) => {
         if (todo.id === id) {
           return {
             ...todo, //Spread Operator to spread the properties of the item
@@ -37,58 +18,53 @@ class TodoContainer extends React.Component {
           };
         }
         return todo;
-      }),
-    }));
+      })
+    );
   };
 
-  addTodoItem = (title) => {
+  const addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
       title: title,
       completed: false,
     };
-    this.setState({
-      todos: [...this.state.todos, newTodo],
-    });
+    setTodos([...todos, newTodo]);
   };
 
-  deleteTodo = (id) => {
-    this.setState({
-      todos: [
-        ...this.state.todos.filter((todo) => {
-          //filter returns a new array by applying a condition on every array element
-          return todo.id !== id;
-        }),
-      ],
-    });
+  const deleteTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        //filter returns a new array by applying a condition on every array element
+        return todo.id !== id;
+      }),
+    ]);
   };
 
-  setUpdate = (updatedTitle, id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
         if (todo.id === id) {
           todo.title = updatedTitle;
         }
         return todo;
-      }),
-    });
+      })
+    );
   };
 
-  render() {
-    return (
-      <div className="container">
-        <div className="inner">
-          <Header />
-          <InputTodo addTodoProps={this.addTodoItem} />
-          <TodosList
-            todos={this.state.todos}
-            handleChangeProps={this.handleChange}
-            deleteTodoProps={this.deleteTodo}
-            setUpdate={this.setUpdate}
-          />
-        </div>
+  return (
+    <div className="container">
+      <div className="inner">
+        <Header />
+        <InputTodo addTodoProps={addTodoItem} />
+        <TodosList
+          todos={todos}
+          handleChangeProps={handleChange}
+          deleteTodoProps={deleteTodo}
+          setUpdate={setUpdate}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default TodoContainer;
