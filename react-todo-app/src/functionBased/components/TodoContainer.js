dc7693ce8b21bebcf1c21fbda5fac55b0fed42ad
Simon Garcia <sgarcia@neural.com.do>
2022-09-22 11:17:39 -0400

    Changed project directory structure

---
+++ react-todo-app/src/functionBased/components/TodoContainer.js
@@ -0,0 +1,92 @@
+import React from "react";
+import TodosList from "./TodosList";
+import InputTodo from "./InputTodo";
+import Header from "./Header";
+import {v4 as uuidv4} from "uuid";
+
+class TodoContainer extends React.Component{
+    state = {
+        todos:[]
+    };
+
+    componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if(loadedTodos) {
+            this.setState({
+                todos: loadedTodos
+            })
+        }
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+            const temp = JSON.stringify(this.state.todos)
+            localStorage.setItem("todos", temp)
+        }
+    };
+
+    handleChange = (id) =>{
+        this.setState(prevState => ({ //Updater function
+            todos: prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo, //Spread Operator to spread the properties of the item
+                        completed: !todo.completed,
+                    }
+                }
+                return todo
+            }),
+        }))
+    };
+
+    addTodoItem = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        };
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    };
+
+    delTodo = (id) =>{
+        this.setState({
+            todos: [
+                ...this.state.todos.filter(todo => { //filter returns a new array by applying a condition on every array element
+                    return todo.id !== id;
+                })
+            ]
+        })
+    };
+
+    setUpdate = (updatedTitle, id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if(todo.id === id) {
+                    todo.title = updatedTitle
+                }
+                return todo
+            })
+        })
+    }
+    
+    render() {
+        return (
+            <div className="container">
+                <div className="inner">
+                    <Header />
+                    <InputTodo addTodoProps={this.addTodoItem}/>
+                    <TodosList 
+                        todos={this.state.todos}
+                        handleChangeProps={this.handleChange}
+                        deleteTodoProps={this.delTodo}
+                        setUpdate={this.setUpdate}
+                    />
+                </div>
+            </div>
+        )
+    };
+}
+export default TodoContainer
--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -2,10 +2,10 @@ import React from "react"
 import ReactDom from "react-dom"
 
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./functionBased/components/TodoContainer"
 
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css"
 
 ReactDom.render(
 <React.StrictMode>
