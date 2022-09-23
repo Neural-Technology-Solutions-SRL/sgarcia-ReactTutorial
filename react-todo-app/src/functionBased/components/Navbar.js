b0f25774120e2070f55d7716ac3d8b85d50e0fd2
Simon Garcia <sgarcia@neural.com.do>
2022-09-23 12:51:05 -0400

    Updated router on Navbar

--- react-todo-app/src/functionBased/components/Navbar.js
+++ react-todo-app/src/functionBased/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
         {links.map((link) => {
           return (
             <li key={link.id}>
-              <NavLink to={link.path} activeClassName="active-link" exact>
+              <NavLink to={link.path} className="active-link" end>
                 {link.text}
               </NavLink>
             </li>
